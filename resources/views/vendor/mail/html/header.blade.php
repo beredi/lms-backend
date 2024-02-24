0d8ee7e9b6e1cb2911a03852347658559f29902d
beredi <43635436+beredi@users.noreply.github.com>
2024-02-24 15:29:41 +0100

    fix mail header image

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('storage/mail-header.jpg') }}" alt="Header Image">
+            <img src="{{ asset('storage/app/public/mail-header.jpg') }}" alt="Header Image">
         </a>
     </td>
 </tr>
